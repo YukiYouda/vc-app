03d1c8a73eeee19902158a18fa156fe4df1b34bb
YukiYouda <youda0518@yahoo.co.jp>
2021-09-12 13:08:56 +0900

    VCApp完成！！！かな？

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    public function hello ()
+    {
+        return view('message.hello');
+    }
+    public function var ()
+    {
+        $variable = 'Hello from web.php';
+        return view('message.var', ['variable' => $variable]);
+    }
+    public function word ($msg)
+    {
+        return view('message.word', ['msg' => $msg]);
+    }
+    public function word2 ($name, $msg)
+    {
+        return view('message.word2', [
+        'name' => $name,
+        'msg' => $msg
+    ]);
+    }
 }
